a0376b4e1c54fb65d04073263b638658c220816f
Deep Patel <pateldeep494@gmail.com>
2022-01-22 20:40:12 -0500

    Rough outline for objects

---
+++ include/asteroids.hpp
@@ -0,0 +1,27 @@
+namespace as
+{
+
+    class Asteroid
+    {
+        public: 
+
+        private:
+
+    };
+
+    class SpaceShip
+    {
+        public:
+
+        private:
+    };
+
+    class Missile
+    {
+        public:
+
+        private:
+
+    };
+
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,30 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 
+#include <asteroids.hpp>
+
 int main()
 {
+    // globally enable antialiasing, make the edges of shapes smoother
+    // (if supported by the grahpics card)
+    sf::ContextSettings settings;
+    settings.antialiasingLevel = 8;
+
+    // get desktop width and height
     auto screenWidth = sf::VideoMode::getDesktopMode().width;
     auto screenHeight = sf::VideoMode::getDesktopMode().height;
-    sf::RenderWindow window(sf::VideoMode(screenWidth, screenHeight), "Asteroids");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+
+    // init window
+    sf::RenderWindow window(
+        sf::VideoMode(screenWidth, screenHeight),
+        "Asteroids",
+        sf::Style::Default,
+        settings);
+
+    sf::CircleShape spaceship(0.02f * screenWidth, 3);
+    spaceship.setFillColor(sf::Color::Green);
+    spaceship.setOrigin(sf::Vector2f(spaceship.getRadius(), spaceship.getRadius()));
+    spaceship.setPosition(sf::Vector2f(screenWidth / 2.0f - spaceship.getRadius(), screenHeight / 2.0f - spaceship.getRadius()));
 
     while (window.isOpen())
     {
@@ -18,10 +35,10 @@ int main()
                 window.close();
         }
 
-        window.clear();
-        window.draw(shape);
+        window.clear(sf::Color::Black);
+        window.draw(spaceship);
         window.display();
     }
 
-    return 0;
+    return EXIT_SUCCESS;
 }
